9115ef011d781cecc99ea9262670f9dd2aaa934b
fefex302 <83668268+fefex302@users.noreply.github.com>
2021-05-13 22:40:02 +0200

    Create utils.h

---
+++ includes/utils.h
@@ -0,0 +1,24 @@
+#if !defined(UTILS)
+#define UTILS
+
+//#define _POSIX_C_SOURCE 200112L
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+static int isNumber(const char* s, long* n) {
+  if (s==NULL) return 1;
+  if (strlen(s)==0) return 1;
+  char* e = NULL;
+  errno=0;
+  long val = strtol(s, &e, 10);
+  if (errno == ERANGE) return 2;    // overflow/underflow
+  if (e != NULL && *e == (char)0) {
+    *n = val;
+    return 0;   // successo 
+  }
+  return 1;   // non e' un numero
+}
+
+#endif
