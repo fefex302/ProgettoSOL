268ed7b84a65fb9dd13955d853138b0adf5997d2
fefex302 <83668268+fefex302@users.noreply.github.com>
2021-05-20 08:21:09 +0200

    Update api.h

--- includes/api.h
+++ includes/api.h
@@ -1,97 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <stdarg.h>
-#include <dirent.h>
-#include <time.h>
-#include <assert.h>
-#include <errno.h>
+#if !defined(API)
+#define API
 
-#include <pwd.h>
-#include <grp.h>
-#include <stddef.h>
+int openConnection(const char* sockname, int msec,const struct timespec abstime);
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include "api.h"
+int closeConnection(const char* sockname);
 
-#define NULL_EXIT(X,str)	\
-  if ((X)==NULL) {			\
-    perror(#str);			\
-    exit(EXIT_FAILURE);			\
-  }
+int openFile(const char* pathname, int flags);
 
-#define ZERO_EXIT(X,str)	\
-  if ((X)==0) {			\
-    perror(#str);			\
-    exit(EXIT_FAILURE);			\
-  }
+int readFile(const char* pathname, void** buf, size_t* size);
 
-#define MINUS_ONE_EXIT(X,str)\
-  if ((X)==-1) {			\
-    perror(#str);			\
-    exit(EXIT_FAILURE);			\
-  }
+int readNFiles(int N, const char* dirname);
 
-static int client_fd;
-static struct sockaddr_un server_address;
+int appendToFile(const char* pathname, void* buf, size_t size, const char* dirname);
 
+int lockFile(const char* pathname);
 
-int openConnection( const char* sockname, int msec,const struct timespec abstime ){
+int unlockFile(const char* pathname);
 
-    if(!sockname || (msec < 0)){
-        errno = EINVAL;
-        return -1;
-    }
+int closeFile(const char* pathname);
 
-    if((client_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
-        return -1;
+int removeFile(const char* pathname);
 
-    memset(&server_address, '0', sizeof(server_address));
-    server_address.sun_family = AF_UNIX;
-    strncpy(server_address.sun_path, sockname, strlen(sockname)+1);
-
-    struct timespec sleeptime;
-    sleeptime.tv_sec = 0;
-    sleeptime.tv_nsec = msec * 1000000;
-    struct timespec time_request;
-    time_request.tv_sec = 0;
-    time_request.tv_nsec = msec * 1000000;
-
-    struct timespec current_time;
-    memset(&current_time, '0', sizeof(current_time));
-
-    do{
-        if(connect(client_fd, (struct sockaddr*) &server_address, sizeof(server_address)) != -1)
-            return 0;
-
-        nanosleep(&sleeptime, &time_request);
-
-        if(clock_gettime(CLOCK_REALTIME, &current_time) == -1)
-            return -1;
-
-    }while(current_time.tv_sec < abstime.tv_sec ||
-        current_time.tv_nsec < abstime.tv_nsec);
-
-    return 0;
-}
-
-
-int closeConnection( const char* sockname ){
-    if(!sockname){
-        errno = EINVAL;
-        return -1;
-    }
-
-    if(strncmp(server_address.sun_path, sockname, strlen(sockname)+1) ==  0){
-        return close(client_fd);
-    }else{
-        errno = EFAULT;
-        return -1;
-    }
-}
+#endif
