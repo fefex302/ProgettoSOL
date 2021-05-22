e92d47c95ad62e351bec9a18646d94b110291064
fefex302 <83668268+fefex302@users.noreply.github.com>
2021-05-22 10:56:25 +0200

    Update api.c

--- src/api.c
+++ src/api.c
@@ -22,6 +22,18 @@
 #include "api.h"
 #include "utils.h"
 
+#define OPN 	1;	//open
+#define OPNC 	2;	//open create mode
+#define OPL 	3;	//open lock mode
+#define OPNL 	4;	//open create and lock mode
+#define RD 		5;	
+#define RDN 	4;
+#define APP 	5;
+#define LO 		6;
+#define UN 		7;
+#define CLS 	8;
+#define RM 		9;
+
 #define NULL_EXIT(X,str)	\
   if ((X)==NULL) {			\
     perror(#str);			\
@@ -40,25 +52,33 @@
     exit(EXIT_FAILURE);			\
   }
 
+#define MINUS_ONE_RETURN(X,str)\
+  if ((X)==-1) {			\
+    perror(#str);			\
+    return -1;			\
+  }
+
 static int client_fd;
 static int connection_active = 0;
 static struct sockaddr_un server_address;
+size_t gvar = 0;
 
 //*************************************FUNZIONI STATICHE DI UTILITÀ***************************************************************
-static int read_file(int fd_file, char *file_path, char *file_return){		//legge un file e restituisce nel campo file_return 
-	fd_file = open(file_path,O_RDONLY);								//una stringa contenente tutti i bit del file
-	struct stat buf;
-	MINUS_ONE_EXIT(fstat(fd_file, &buf),"fstat");
-
-	size_t size = buf.st_size;
-
-	NULL_EXIT(file_return = malloc(size),"malloc");
+static int read_file(int fd_file,const char *file_path, char *file_return, size_t *size){		//legge un file e restituisce nel campo file_return 
+	fd_file = open(file_path,O_RDONLY);															//una stringa contenente tutti i bit del file
 	
-	return readn(fd_file,file_return,size);
+	struct stat buf;
+	MINUS_ONE_RETURN(fstat(fd_file, &buf),"fstat");
+
+	*size = buf.st_size;
+
+	file_return = malloc(buf.st_size);
+	
+	return readn(fd_file, file_return, buf.st_size);
 }
 
 
-static int open_file(char *file_to_read,char *path_to_dir){		//legge il file contenuto nella stringa e se una cartella
+static int open_file(const char *file_to_read,char *path_to_dir){		//legge il file contenuto nella stringa e se una cartella
 	if(path_to_dir == NULL)									//di salvataggio è settata allora crea un file in quella cartella
 		return 0;		
 
@@ -99,8 +119,10 @@ int openConnection( const char* sockname, int msec,const struct timespec abstime
     memset(&current_time, '0', sizeof(current_time));
 
     do{
-        if(connect(client_fd, (struct sockaddr*) &server_address, sizeof(server_address)) != -1)
+        if(connect(client_fd, (struct sockaddr*) &server_address, sizeof(server_address)) != -1){
+        	connection_active = 1;
             return 0;
+        }
 
         nanosleep(&sleeptime, &time_request);
 
@@ -133,14 +155,53 @@ int closeConnection( const char* sockname ){
 
 //EPERM: connessione non attiva
 //EINVAL: sto passando un pathname nullo
+
 int openFile(const char* pathname, int flags){
+
 	if(!connection_active){
 		errno = EPERM;
+		printf("eperm\n");
 		return -1;
 	}
+
 	if(pathname == NULL){
 		errno = EINVAL;
-		return -1
+		printf("einval\n");
+		return -1;
 	}
-	
+
+	int request;
+
+	if(flags == O_CREAT){
+		request = OPNC;
+		printf("request\n");
+		if(writen(client_fd, &request, sizeof(request)) == -1)		//mando il tipo di richiesta
+			return -1;
+
+		int lenght = strlen(pathname)+1;
+		if(writen(client_fd, &lenght, sizeof(int)) == -1)		//mando la lunghezza del pathname
+			return -1;
+
+		if(writen(client_fd, pathname, lenght) == -1)				//mando il pathname
+			return -1;
+
+		int fd_file;
+		char* file_to_send;
+		size_t size;
+
+		if(read_file(fd_file, pathname, file_to_send, &size) == -1)
+			return -1;
+
+		printf("dim file client %s\n",strlen(file_to_send));
+		if(writen(client_fd, &size, sizeof(size)) == -1)			//mando la lunghezza del file
+			return -1;
+
+
+		if(writen(client_fd, file_to_send, size) == -1)				//mando il file
+			return -1;
+
+		gvar = size;
+		return 0;
+
+	}
 }
