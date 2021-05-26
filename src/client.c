7e458cf950d7a46d6424007abe25a45bc1a2cf43
fefex302 <83668268+fefex302@users.noreply.github.com>
2021-05-27 00:34:28 +0200

    Update client.c

--- src/client.c
+++ src/client.c
@@ -20,8 +20,7 @@
 #define MSEC 10
 
 static int flag_f = 0;	//variabile che identifica se la connessione col socket è stata effettuata o meno
-static int flag_p = 0;	//variabile che identifica se la stampa delle operazioni è abilitata
-extern size_t gvar;
+extern int print_flag;	//variabile che identifica se la stampa delle operazioni è abilitata
 
 #define NULL_EXIT(X,str)	\
   if ((X)==NULL) {			\
@@ -44,131 +43,130 @@ int parse_arguments(char *buf,char *buf2[]);
 int help();
 int f_req(char* args);
 int disconnect(char *args);
-int W_req(char* args);
+int W_req(char *args,char *dirname);
+
+
+//*******************************************MAIN*****************************************************************************
 int main(int argc,char* argv[]){
 
+	if(argc == 1){
+		printf("usare almeno un argomento, lanciare con opzione -h per vedere le richieste possibili\n");
+		return 0;
+	}
+
 	int opt;
-	int n;
-	char buffer[BUF_LEN];
-	char *string_buffer[BUF_LEN_2];
-	string_buffer[0] = "client_requests";
-	for(int i =0; i<BUF_LEN_2; i++)
-		string_buffer[i] = NULL;
 	int end = 0;	//usato per terminare il client
-	int restart = 0;	//usato per ricominciare il parse degli argomenti quando leggo qualcosa di sbagliato
+
 	char *sockname = NULL;
+	char *dirname = NULL;	//cartella dove memorizzare i file letti dal server	
 
-	while(!end){
-		restart = 0;
-		memset(buffer,'\0',sizeof(char));
-		if(fgets(buffer,BUF_LEN,stdin) == NULL)
-			continue;
+	int d=0,r=0,R=0,h=0,f=0,p=0;
 
-		n = parse_arguments(buffer,string_buffer);
-		printf("n = %d\n",n);
+	for(int i = 1; i<argc; i++){
+		if(strncmp(argv[i],"-d",3) == 0)
+			d = i;
+		else if(strcmp(argv[i],"-r") == 0)
+			r = i;
+		else if(strcmp(argv[i],"-R") == 0)
+			R = i;
+		else if(strcmp(argv[i],"-h") == 0)
+			h = i;
+		else if(strcmp(argv[i],"-f") == 0)
+			f = i;
+		else if(strcmp(argv[i],"-p") == 0)
+			p = i;
+	}
+	if(h > 0){
+		help();
+		return 0;
+	}
 
-		/*for(int i = 0; i<BUF_LEN_2; i++){
-			if(string_buffer[i] == NULL)
-				printf("%d = NULL\n",i);
-			else 
-				printf("%d = %s\n",i,string_buffer[i]);
-		}*/
-		int d=0,r=0,R=0;
+	if(f > 0){
+		f++;
+		printf("tentativo di connessione con il socket << %s >>\n",argv[f]);
+		if(f_req(argv[f]) != 0){
+			printf("connessione con il socket << %s >> fallita\n",argv[f]);
+			return 0;
+		}
+		else{
+			printf("connessione con il socket << %s >> stabilita\n",argv[f]);
+			sockname = argv[f];
+		}
+	}
+	else{
+		printf("prima di svolgere operazioni è necessario stabilire prima una connessione con il server\nusare -f nomesocket\n");
+		return 0;
+	}
 
-		for(int i = 1; i<BUF_LEN_2; i++){
-			if(string_buffer[i] == NULL)
+
+	if(d>0){
+		if(r>0){
+			r++;
+			dirname = argv[r];
+		}
+		else if(R>0){
+			R++;
+			dirname = argv[R];
+		}
+		else{
+			printf("-d needs at least one -r or -R\n");
+			end = 1;
+		}
+	}
+
+	//abilito le stampe delle operazioni
+	if(p>0){
+		print_flag = 1;
+	}
+		
+	opterr = 0;
+
+	while ((opt = getopt(argc,argv, ":hf:w:W:r:R:d:t:l:u:c:p")) != -1 && !end) {
+		switch(opt) {
+			case 'h':  
 				break;
-			else if(strncmp(string_buffer[i],"-d",3) == 0)
-				d = i;
-			else if(strcmp(string_buffer[i],"-r") == 0)
-				r = i;
-			else if(strcmp(string_buffer[i],"-R") == 0)
-				R = i;
-			printf("d = %d,r = %d, R = %d\n",d,r,R);
-		}
-		if(d>0){
-			if(r>0)
-				printf("ok\n");
-			if(R>0)
-				printf("ok\n");
-			else{
-				printf("-d needs at least one -r or -R\n");
-				restart = 1;
-			}
-		}
-		optind = 0;
-		opterr = 0;
-
-		while ((opt = getopt(n,string_buffer, ":hf:w:W:r:R:d:t:l:u:c:p")) != -1 && !end && !restart) {
-		    switch(opt) {
-				case 'h':  
-					help();
-					end = 1;
-					break;
-				case 'f':
-					if(flag_p)
-						printf("tentativo di connessione con il socket %s\n",optarg);
-					if(!flag_f){
-						if(f_req(optarg)== 0){
-							flag_f = 1;
-							NULL_EXIT(sockname = malloc(strlen(optarg)+1),"malloc");
-						}
-						else{
-							printf("connessione con il server fallita\n");
-							restart = 1;
-						}
-					}
-					else{
-						printf("connessione con il server già effettuata\n");
-					}
-					break;
-				case 'w': //arg_o(optarg);  
-					break;
-				case 'W': 
-					W_req(optarg);
-					break;
-				case 'r': //arg_h(argv[0]); 
-					break;
-				case 'R': //arg_h(argv[0]);
-					printf("R con argomenti\n");
-					break;
-				case 'd': //arg_h(argv[0]); 
-					break;
-				case 't': //arg_h(argv[0]); 
-					break;
-				case 'l': //arg_h(argv[0]); 
-					break;
-				case 'u': //arg_h(argv[0]); 
-					break;
-				case 'c': //arg_h(argv[0]); 
-					break;
-				case 'p': //arg_h(argv[0]); 
-					flag_p = 1;
-					break;
-				case ':': 
-					switch(optopt){
-						case 'R':
-							printf("ciaoooooo\n");
-							break;
-						case 't':
-							printf("t senza argomenti\n");
-							break;
-						default:
-			   				printf("l'opzione '-%c' richiede un argomento\n", optopt);
-				    } break;
-				case '?': {  // restituito se getopt trova una opzione non riconosciuta
-				      printf("l'opzione '-%c' non e' gestita\n", optopt);
+			case 'f':
+				break;
+			case 'w':   
+				break;
+			case 'W': 
+				W_req(optarg,dirname);
+				break;
+			case 'r':  
+				break;
+			case 'R': 
+				printf("R con argomenti\n");
+				break;
+			case 'd': 
+				break;
+			case 't':  
+				break;
+			case 'l':  
+				break;
+			case 'u':  
+				break;
+			case 'c':  
+				break;
+			case 'p': 
+				break;
+			case ':': 
+				switch(optopt){
+					case 'R':
+						printf("ciaoooooo\n");
+						break;
+					case 't':
+						printf("t senza argomenti\n");
+						break;
+					default:
+			   			printf("l'opzione '-%c' richiede un argomento\n", optopt);
+				} break;
+			case '?': {  // restituito se getopt trova una opzione non riconosciuta
+				printf("l'opzione '-%c' non e' gestita\n", optopt);
 				    } break;
 				default:;
-    		}
-  		}
-  		for(int i=1; i<n; i++){
-  				if(string_buffer[i]){
-                free(string_buffer[i]);
-                string_buffer[i] = NULL;
-         	}
-        }
-	}
+    	}
+  	}
+  	sleep(2);
 	if(sockname != NULL)
 		disconnect(sockname);
   	return 0;
@@ -217,7 +215,7 @@ int help(){
 			"-wdirname[,n=0]",
 			"-W file1[,file2]",
 			"-r file1[,file2]",
-			"-R[n=0]",
+			"-R [n=0]",
 			"-d dirname",
 			"-t time",
 			"-l file1[,file2]",
@@ -235,7 +233,6 @@ int f_req(char* args){
 	
 	char *s = malloc(strlen(args)+1);
 	strncpy(s,args,strlen(args)+1);
-	printf("%s\n",s);
 	if(openConnection(s,MSEC,abstime) == -1){
 		free(s);
 		return -1;
@@ -246,27 +243,31 @@ int f_req(char* args){
 
 
 int disconnect(char *args){
-	return closeConnection(args);
+	if(print_flag)
+		printf("richiesta di chiusura della connessione\n");
+	if(closeConnection(args) != -1){
+		if(print_flag)
+			printf("connessione chiusa\n");
+		return 0;
+	}
+	else {
+		if(print_flag)
+			printf("richiesta di chiusura della connessione fallita\n");
+		return -1;
+	}
+
 }
 
-int W_req(char *args){				//args lista di file da scrivere separati da virgola
-									//buffer che mi conterrà tutti i nomi dei file da passare
+int W_req(char *args,char *dirname){				//args lista di file da scrivere separati da virgola
+									
 	char *tmpstr;													
 	char *token = strtok_r(args, ",", &tmpstr);
 	int i = 0;
 	
 	while (token) {
-		if(flag_p)
-			printf("richiesta di scrittura del file <%s>\n",token);
 
-		if(openFile(token,O_CREAT) == 0){
-			if(flag_p){
-				printf("la richiesta di scrittura del file <%s> ha avuto successo\n",token);
-				printf("numero di bytes scritti: %d\n",gvar);
-			}
-		}
-		else{
-			if(flag_p)
+		if(writeFile(token,dirname) != 0){		
+			if(print_flag)
 				printf("richiesta di scrittura del file <%s> è fallita\n",token);
 		}
 
