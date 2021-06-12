1119d65de306db817c5784f514e67ca161cec015
fefex302 <83668268+fefex302@users.noreply.github.com>
2021-06-12 15:50:41 +0200

    Update api.h

--- includes/api.h
+++ includes/api.h
@@ -1,9 +1,30 @@
 #if !defined(API)
 #define API
 
+//richieste mandabili al server sottoforma di numeri
+#define OPN 	1	//open
+#define OPNC 	2	//open create mode
+#define OPNL 	3	//open lock mode
+#define OPNCL 	4	//open create and lock mode
+#define RD 		5	
+#define RDN 	6
+#define APP 	7
+#define LO 		8
+#define UN 		9
+#define CLS 	10
+#define RM 		11
+#define CLOSE 	12
+#define WRT		13
+
+#define O_CREATE 1
+#define O_LOCK 2
+#define O_LOCK_CREATE 3
+
 int openConnection(const char* sockname, int msec,const struct timespec abstime);
 
 int closeConnection(const char* sockname);
+
+int writeFile(const char* pathname, const char*dirname);
 
 int openFile(const char* pathname, int flags);
 
